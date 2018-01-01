63d081180fa1954878c862a20768ee4965dcb8ee
Kris Hunt <sourcekris@users.noreply.github.com>
2018-01-01 16:25:31 +1100

    Added ANSI colors and basic terminal control.

--- certstreamcatcher.go
+++ certstreamcatcher.go
@@ -10,6 +10,13 @@ import (
   "github.com/CaliDog/certstream-go" // certstream api library
 )
 
+const (
+  RL = "\r\033[K"
+  RED = "\033[31m\033[4m"
+  YEL = "\033[33m\033[4m"
+  RES = "\033[0m"
+)
+
 var (
   buf bytes.Buffer
   logger = log.New(&buf, "certstream: ", log.Lshortfile)
@@ -27,106 +34,18 @@ var (
                   "business", "com"}
 )
 
-// isPhishing checks if a domainList contains phishing domains. 
-func (dl *domainList) isPhishing() {
-  
-  var suspicious = false
-
-  // Free certificates are more likely to be used for phishing
-  for _, subject := range dl.subjects {
-    if freeCaRe.MatchString(subject) {
-      suspicious = true
-    }
-  }
-
-  for _, dp := range dl.domains {
-    // If we didnt extract a domain or subdomain, then continue.
-    if isEmpty(dp.domain) || isEmpty(dp.subdomain) {
-      continue
-    }
-  
-    // Extract the keywords from the dp maps.
-    keywords := getKeywords(dp.raw, phishingRe)
-    domainKeywords := getKeywords(dp.domain, phishingRe)
-    subDomainKeywords := getKeywords(dp.subdomain, phishingRe)
-
-    // dashes := len(getKeywords(dp.raw, dashes))
-    // dots := len(getKeywords(dp.raw, dots))
-
-    if dp.hasPunycode {
-      punycodeKeywords := phishingRe.FindAllString(dp.raw["ascii"], -1)
-
-      if len(keywords) > 0 && len(punycodeKeywords) > 0 {
-        fmt.Printf("[!] Punycode %s = %s (%s, %s)\n", 
-                   dp.raw["original"], dp.raw["ascii"], 
-                   keywords, punycodeKeywords)
-      }
-
-      return
-    }
-
-    if len(tlds) > 0 {
-      // Only return results when we see a tld in our list.
-      if dp.containsTld(tlds) {
-        if suspicious {
-          if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
-            fmt.Printf("[!] Suspicious %s\n", dp.raw["original"])
-          }
-
-          if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
-            fmt.Printf("[!] Likely %s\n", dp.raw["original"])
-          }
-        } else {
-          if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
-            fmt.Printf("[!] Likely %s\n", dp.raw["original"])
-          }
-
-          if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
-            fmt.Printf("[!] Potential %s\n", dp.raw["original"])
-          }
-        }
-      }
-    } else {
-      if suspicious {
-        if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
-          fmt.Printf("[!] Suspicious %s\n", dp.raw["original"])
-        }
-
-        if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
-          fmt.Printf("[!] Likely %s\n", dp.raw["original"])
-        }
-      } else {
-        if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
-          fmt.Printf("[!] Likely %s\n", dp.raw["original"])
-        }
-
-        if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
-          fmt.Printf("[!] Potential %s\n", dp.raw["original"])
-        }
-      }
-    }  
-  }
-}
-
-func sumArray (a []int) int {
-  var result int
-  for _, v := range a {
-    result = result + v
-  }
-
-  return result
-}
-
 func main() {
+  var count int
 
   logger.SetOutput(os.Stderr)
 
-  //re := regexp.MustCompile(regex)
-
   stream, errStream := certstream.CertStreamEventStream(true)
+  fmt.Printf("Connection established to certstream! Waiting for messages...\n")
   for {
     select {
       case jq := <-stream:
+        count++
+        fmt.Printf("%s%d Certs", RL, count)
         dl, err := newDomainList(jq)
         if err != nil{
           logger.Printf(err.Error())
@@ -139,7 +58,6 @@ func main() {
               
       case err := <-errStream:
         logger.Print(err)
-        fmt.Print(&buf)
     }
   }
 }
---
+++ phishing.go
@@ -0,0 +1,102 @@
+/*
+ * phising.go implements the phishing detection logic.
+ */ 
+
+ package main
+
+import (
+  "fmt"
+)
+
+ // isPhishing checks if a domainList contains phishing domains. 
+func (dl *domainList) isPhishing() {
+  
+  var suspicious = false
+
+  // Free certificates are more likely to be used for phishing
+  for _, subject := range dl.subjects {
+    if freeCaRe.MatchString(subject) {
+      suspicious = true
+    }
+  }
+
+  for _, dp := range dl.domains {
+    // If we didnt extract a domain or subdomain, then continue.
+    if isEmpty(dp.domain) || isEmpty(dp.subdomain) {
+      continue
+    }
+  
+    // Extract the keywords from the dp maps.
+    keywords := getKeywords(dp.raw, phishingRe)
+    domainKeywords := getKeywords(dp.domain, phishingRe)
+    subDomainKeywords := getKeywords(dp.subdomain, phishingRe)
+
+    // This is never used in the nodejs version either. Seems to be useful
+    // though since many phishing subdomains are long strings of hyphenated
+    // words.
+    // dashes := len(getKeywords(dp.raw, dashes))
+    // dots := len(getKeywords(dp.raw, dots))
+
+    if dp.hasPunycode {
+      punycodeKeywords := phishingRe.FindAllString(dp.raw["ascii"], -1)
+
+      if len(keywords) > 0 && len(punycodeKeywords) > 0 {
+        fmt.Printf("%s[!] Punycode %s%s%s = %s (%s, %s)\n", RL, RED, 
+                   dp.raw["original"], RES, dp.raw["ascii"], 
+                   keywords, punycodeKeywords)
+      }
+
+      return
+    }
+
+    if len(tlds) > 0 {
+      if dp.containsTld(tlds) {
+        // Only return results when we see a tld in our list.
+        if suspicious {
+          if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
+            fmt.Printf("%s[!] Suspicious %s%s%s\n", RL, RED, dp.raw["original"], RES)
+            return
+          }
+
+          if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
+            fmt.Printf("%s[!] Likely %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+            return
+          }
+        } else {
+          if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
+            fmt.Printf("%s[!] Likely %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+            return
+          }
+
+          if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
+            fmt.Printf("%s[!] Potential %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+            return
+          }
+        }
+      }
+    } else {
+      // Return results for all TLDs
+      if suspicious {
+        if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
+          fmt.Printf("%s[!] Suspicious %s%s%s\n", RL, RED, dp.raw["original"], RES)
+          return
+        }
+
+        if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
+          fmt.Printf("%s[!] Likely %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+          return
+        }
+      } else {
+        if hasKeywordsPrefix(keywords, subDomainKeywords) && len(subDomainKeywords) >= 1 {
+          fmt.Printf("%s[!] Likely %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+          return
+        }
+
+        if hasKeywordsPrefix(keywords, domainKeywords) && len(domainKeywords) >= 1 {
+          fmt.Printf("%s[!] Potential %s%s%s\n", RL, YEL, dp.raw["original"], RES)
+          return
+        }
+      }
+    }  
+  }
+}
